5b64bdac99f14b8567040b0343f0425214cb1c8d
first commit <stebataras@gmail.com>
2024-06-13 23:00:41 +0200

    New form is load data to DB

--- data.php
+++ data.php
@@ -1,24 +1,69 @@
 <?php
 require_once("db.php");
 
-$imie = $_POST['imie'];
-$nazwisko = $_POST['nazwisko'];
-$przyjazd = $_POST['przyjazd'];
-$odjazd = $_POST['odjazd'];
-$adres = $_POST['adres'];
-$zaklad = $_POST['zaklad'];
-$spolka = $_POST['spolka'];
-$administrator = $_POST['administrator'];
+// Проверяем, был ли запрос выполнен методом POST
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-$sql = "INSERT INTO `work` (imie, nazwisko, przyjazd, adres, zaklad, spolka, administrator) VALUES ('$imie','$nazwisko','$przyjazd','$adres','$zaklad','$spolka','$administrator')";
+    // Получаем значения из $_POST с учетом возможности NULL и экранируем их
+    $imie = isset($_POST['imie']) ? mysqli_real_escape_string($conn, $_POST['imie']) : NULL;
+    $nazwisko = isset($_POST['nazwisko']) ? mysqli_real_escape_string($conn, $_POST['nazwisko']) : NULL;
+    $przyjazd = isset($_POST['przyjazd']) ? mysqli_real_escape_string($conn, $_POST['przyjazd']) : NULL;
+    $odjazd = isset($_POST['odjazd']) ? mysqli_real_escape_string($conn, $_POST['odjazd']) : NULL;
+    $adres = isset($_POST['adres']) ? mysqli_real_escape_string($conn, $_POST['adres']) : NULL;
+    $zaklad = isset($_POST['zaklad']) ? mysqli_real_escape_string($conn, $_POST['zaklad']) : NULL;
+    $spolka = isset($_POST['spolka']) ? mysqli_real_escape_string($conn, $_POST['spolka']) : NULL;
+    $administrator = isset($_POST['administrator']) ? mysqli_real_escape_string($conn, $_POST['administrator']) : NULL;
+    $narodowosc = isset($_POST['narodowosc']) ? mysqli_real_escape_string($conn, $_POST['narodowosc']) : NULL;
+    $data_urodzenia = isset($_POST['data-urodzenia']) ? mysqli_real_escape_string($conn, $_POST['data-urodzenia']) : NULL;
+    $wiek = isset($_POST['wiek']) ? mysqli_real_escape_string($conn, $_POST['wiek']) : NULL;
+    $numer_paszportu = isset($_POST['numer-paszportu']) ? mysqli_real_escape_string($conn, $_POST['numer-paszportu']) : NULL;
+    $numer_pesel = isset($_POST['numer-pesel']) ? mysqli_real_escape_string($conn, $_POST['numer-pesel']) : NULL;
+    $telefon = isset($_POST['telefon']) ? mysqli_real_escape_string($conn, $_POST['telefon']) : NULL;
+    $student = isset($_POST['student']) ? mysqli_real_escape_string($conn, $_POST['student']) : NULL;
+    $legitymacja = isset($_POST['legitymacja']) ? mysqli_real_escape_string($conn, $_POST['legitymacja']) : NULL;
+    $stanowisko = isset($_POST['stanowisko']) ? mysqli_real_escape_string($conn, $_POST['stanowisko']) : NULL;
+    $badanie = isset($_POST['badanie']) ? mysqli_real_escape_string($conn, $_POST['badanie']) : NULL;
+    $bhp = isset($_POST['bhp']) ? mysqli_real_escape_string($conn, $_POST['bhp']) : NULL;
+    $zgloszenie_wyjazdu = isset($_POST['zgloszenie-wyjazdu']) ? mysqli_real_escape_string($conn, $_POST['zgloszenie-wyjazdu']) : NULL;
+    $zezwol_oswiad = isset($_POST['zezwol-oswiad']) ? mysqli_real_escape_string($conn, $_POST['zezwol-oswiad']) : NULL;
+    $waznosc_zezw = isset($_POST['waznosc-zezw']) ? mysqli_real_escape_string($conn, $_POST['waznosc-zezw']) : NULL;
+    $wiza_bio = isset($_POST['wiza-bio']) ? mysqli_real_escape_string($conn, $_POST['wiza-bio']) : NULL;
+    $waznosc_wisa = isset($_POST['waznosc-wisa']) ? mysqli_real_escape_string($conn, $_POST['waznosc-wisa']) : NULL;
+    $karta_pobytu = isset($_POST['karta-pobytu']) ? mysqli_real_escape_string($conn, $_POST['karta-pobytu']) : NULL;
+    $waznosc_karta = isset($_POST['waznosc-karta']) ? mysqli_real_escape_string($conn, $_POST['waznosc-karta']) : NULL;
+    $status_karta = isset($_POST['status-karta']) ? mysqli_real_escape_string($conn, $_POST['status-karta']) : NULL;
+    $uwagi = isset($_POST['uwagi']) ? mysqli_real_escape_string($conn, $_POST['uwagi']) : NULL;
+    $komentarz = isset($_POST['komentarz']) ? mysqli_real_escape_string($conn, $_POST['komentarz']) : NULL;
+    $info = isset($_POST['info']) ? mysqli_real_escape_string($conn, $_POST['info']) : NULL;
 
-$result = mysqli_query($conn, $sql);
+    // Подготавливаем значения даты для SQL-запроса
+    $data_urodzenia_sql = $data_urodzenia ? "'$data_urodzenia'" : "NULL";
+    $legitymacja_sql = $legitymacja ? "'$legitymacja'" : "NULL";
+    $badanie_sql = $badanie ? "'$badanie'" : "NULL";
+    $bhp_sql = $bhp ? "'$bhp'" : "NULL";
+    $zgloszenie_wyjazdu_sql = $zgloszenie_wyjazdu ? "'$zgloszenie_wyjazdu'" : "NULL";
+    $waznosc_zezw_sql = $waznosc_zezw ? "'$waznosc_zezw'" : "NULL";
+    $wiza_bio_sql = $wiza_bio ? "'$wiza_bio'" : "NULL";
+    $waznosc_wisa_sql = $waznosc_wisa ? "'$waznosc_wisa'" : "NULL";
+    $waznosc_karta_sql = $waznosc_karta ? "'$waznosc_karta'" : "NULL";
+    $odjazd_sql = $odjazd ? "'$odjazd'" : "NULL";
+    
 
-if ($result) {
-    echo json_encode(["message" => "Data inserted successfully."]);
+    // Создаем SQL-запрос для вставки данных
+    $sql = "INSERT INTO `work` (imie, nazwisko, przyjazd, odjazd, adres, zaklad, spolka, administrator, narodowosc, `data-urodzenia`, wiek, `numer-paszportu`, `numer-pesel`, telefon, student, legitymacja, stanowisko, badanie, bhp, `zgloszenie-wyjazdu`, `zezwol-oswiad`, `waznosc-zezw`, `wiza-bio`, `waznosc-wisa`, `karta-pobytu`, `waznosc-karta`, `status-karta`, uwagi, komentarz, info) 
+            VALUES ('$imie', '$nazwisko', '$przyjazd', $odjazd_sql, '$adres', '$zaklad', '$spolka', '$administrator', '$narodowosc', $data_urodzenia_sql, '$wiek', '$numer_paszportu', '$numer_pesel', '$telefon', '$student', $legitymacja_sql, '$stanowisko', $badanie_sql, $bhp_sql, $zgloszenie_wyjazdu_sql, '$zezwol_oswiad', $waznosc_zezw_sql, $wiza_bio_sql, $waznosc_wisa_sql, '$karta_pobytu', $waznosc_karta_sql, '$status_karta', '$uwagi', '$komentarz', '$info')";
+
+    // Выполняем SQL-запрос
+    $result = mysqli_query($conn, $sql);
+
+    // Проверяем результат выполнения запроса
+    if ($result) {
+        echo json_encode(["message" => "Data inserted successfully."]);
+    } else {
+        echo json_encode(["error" => "Error: " . $sql . "<br>" . mysqli_error($conn)]);
+    }
 } else {
-    echo json_encode(["error" => "Error: " . $sql . "<br>" . mysqli_error($conn)]);
+    // В случае, если запрос был не методом POST
+    echo json_encode(["error" => "Only POST requests are allowed."]);
 }
 ?>
-
-
--- index.php
+++ index.php
@@ -7,35 +7,69 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <div class="container">
+    <div class="container wrapper-main">
         <div class="wrapper">
             <form id="dataForm" class="form" action="data.php" method="POST">
                 <input class="form__text" type="text" placeholder="imię" name="imie">
                 <input class="form__text" type="text" placeholder="nazwisko" name="nazwisko">
-                <div>
-                    <input class="form__data" type="date" placeholder="przyjazd" name="przyjazd" required>
-                    <input class="form__data" type="date" placeholder="odjazd" name="odjazd">
-                </div>
+                <input class="form__data" type="date" placeholder="przyjazd" name="przyjazd" required>
+                <input class="form__data" type="date" placeholder="odjazd" name="odjazd">
                 <input class="form__text" type="text" placeholder="adres" name="adres">
-                <br>
                 <input class="form__text" type="text" placeholder="zaklad" name="zaklad">
-                <br>
-                <select class="form__text" name="spolka">
+                <select class="form__select" placeholder="Spolka" name="spolka">
+                    <option value="" disabled selected hidden>spolka</option>
                     <option value="APT">APT</option>
                     <option value="WP">WP</option>
                     <option value="SAS">SAS</option>
                     <option value="WD">WD</option>
                     <option value="DIPICO">DIPICO</option>
                 </select>
-                <br>
-                <input class="form__text" type="text" placeholder="administrator" name="administrator">
-                <br>
+                <input class="form__text" type="text" placeholder="Administrator" name="administrator">
+                <input class="form__text" type="text" placeholder="Narodowosc:" name="narodowosc">
+                <input class="form__data" type="date" placeholder="Data urodzenia" name="data-urodzenia">
+                <input class="form__text" type="text" placeholder="Wiek" name="wiek">
+                <input class="form__text" type="text" placeholder="Numer Paszportu" name="numer-paszportu">
+                <input class="form__text" type="text" placeholder="Numer PESEL" name="numer-pesel">
+                <input class="form__text" type="tel" pattern="[0-9]{3} [0-9]{3} [0-9]{3}" placeholder="XXX XXX XXX" name="telefon">
+                <select class="form__select" placeholder="Student" name="student">
+                    <option value="" disabled selected hidden>Student</option>
+                    <option value="APT">TAK</option>
+                    <option value="WP">NIE</option>
+                </select>
+                <input class="form__data" type="date" placeholder="Legitymacja do:" name="legitymacja">
+                <input class="form__text" type="text" placeholder="Stanowisko" name="stanowisko">
+                <input class="form__data" type="date" placeholder="Badanie lekarskie" name="badanie">
+                <input class="form__data" type="date" placeholder="BHP" name="bhp">
+                <input class="form__data" type="date" placeholder="Data zgloszenie wyjazdu" name="zgloszenie-wyjazdu">
+                <select class="form__select" placeholder="Zezwolenie/Oswiadczenie" name="zezwol-oswiad">
+                    <option value="" disabled selected hidden>Zezwol/Oswiad</option>
+                    <option value="APT">ZEZWOLENIE</option>
+                    <option value="WP">OSWIADCZENIE</option>
+                </select>
+                <input class="form__data" type="date" placeholder="Ważność dokumentu (zezw, osw)" name="waznosc-zezw">
+                <select class="form__select" placeholder="Wisa/Bio" name="wiza-bio">
+                    <option value="" disabled selected hidden>Wiza/Bio</option>
+                    <option value="APT">WIZA</option>
+                    <option value="WP">BIO</option>
+                </select>
+                <input class="form__data" type="date" placeholder="Ważność dokumentu (wiza, bio)" name="waznosc-wisa">
+                <select class="form__select" placeholder="Karta Pobytu" name="karta-pobytu">
+                    <option value="" disabled selected hidden>Karta Pobytu</option>
+                    <option value="APT">TAK</option>
+                    <option value="WP">NIE</option>
+                </select>
+                <input class="form__data" type="date" placeholder="Ważność dokumentu (Karta Pobytu)" name="waznosc-karta">
+                <input class="form__text" type="text" placeholder="Status Karty Pobytu" name="status-karta">
+                <input class="form__text" type="text" placeholder="Uwagi" name="uwagi">
+                <input class="form__text" type="text" placeholder="Komentarz" name="komentarz">
+                <input class="form__text" type="text" placeholder="Dodatkowa Informacja" name="info">
                 <div>
                     <button type="submit">ZAPISZ</button>
                 </div>
             </form>
         </div>
     </div>
+        
     <div class="container">
         <select id="tableSelector">
             <option value="table1">List of Workers</option>
@@ -171,18 +205,40 @@
 
     <div class="container">
         <div id="modal" class="modal">
-            <div class="modal-content">
-                <span class="close">&times;</span>
-                <h2>Дополнительная информация</h2>
-                <p><strong>Imię:</strong> <span id="modal-imie"></span></p>
-                <p><strong>Nazwisko:</strong> <span id="modal-nazwisko"></span></p>
-                <p><strong>Przyjazd:</strong> <span id="modal-przyjazd"></span></p>
-                <p><strong>Odjazd:</strong> <span id="modal-odjazd"></span></p>
-                <p><strong>Adres:</strong> <span id="modal-adres"></span></p>
-                <p><strong>Zakład:</strong> <span id="modal-zaklad"></span></p>
-                <p><strong>Spółка:</strong> <span id="modal-spolka"></span></p>
-                <p><strong>Administrator:</strong> <span id="modal-administrator"></span></p>
-            </div>
+            <span class="close">&times;</span>
+            <h2>Дополнительная информация</h2>
+                <div class="modal-content">
+                    <div class="modal__flex" >
+                        <p><strong>Imię:</strong> <span id="modal-imie"></span></p>
+                        <p><strong>Nazwisko:</strong> <span id="modal-nazwisko"></span></p>
+                        <p><strong>Narodowość:</strong> <span id="modal-narodowosc"></span></p>
+                        <p><strong>Data urodzenia:</strong> <span id="modal-urodzenie"></span></p>
+                        <p><strong>Wiek:</strong> <span id="modal-wiek"></span></p>
+                        <p><strong>Numer Paszportu:</strong> <span id="modal-adres"></span></p>
+                        <p><strong>Numer PESEL:</strong> <span id="modal-zaklad"></span></p>
+                        <p><strong>Tel:</strong> <span id="modal-spolka"></span></p>
+                        <p><strong>Student:</strong> <span id="modal-administrator"></span></p>
+                        <p><strong>Legitymacja do:</strong> <span id="modal-administrator"></span></p>
+                        <p><strong>Stanowisko:</strong> <span id="modal-administrator"></span></p>
+                        <p><strong>Badanie lekarske (data):</strong> <span id="modal-administrator"></span></p>
+                        <p><strong>BHP (data):</strong> <span id="modal-administrator"></span></p>
+                        <p><strong>Data zgloshenia wyjazdu:</strong> <span id="modal-administrator"></span></p>
+                    </div>
+                    <div class="modal__flex" >
+                        <p><strong>Typ dokumentu (zezw., osw.):</strong> <span id="modal_zezwol"></span></p>
+                        <p><strong>Ważność dokumentu (zew., osw):</strong> <span id="modal_waznosc-zezw"></span></p>
+                        <p><strong>Typ dokumentu (wiza,bio):</strong> <span id="modal_wiza"></span></p>
+                        <p><strong>Ważność dokumentu (wiza, bio):</strong> <span id="modal_waznosc-wiza"></span></p>
+                        <p><strong>Karta bobytu:</strong> <span id="modal_karta"></span></p>
+                        <p><strong>Wazność karty pobytu:</strong> <span id="modal_waznosc-karta"></span></p>
+                        <p><strong>Status karty pobytu:</strong> <span id="modal_status"></span></p>
+                        <p><strong>Uwagi:</strong> <span id="modal_uwagi"></span></p>
+                        <p><strong>Komentarz:</strong> <span id="modal_komentarz"></span></p>
+                        <p><strong>Dod. Informacja:</strong> <span id="modal_komentarz"></span></p>
+  
+                    </div>
+                </div>
+            <button>SKANY</button>
         </div>
     </div>
 
